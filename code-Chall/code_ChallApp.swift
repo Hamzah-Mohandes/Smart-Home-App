1141fa4ac8c99b015fbde4e91a0d0406c03626dc
Hamzah-Mohandes <rootmoodrm@gmail.com>
2024-10-09 10:58:50 +0200

    Initial Commit

---
+++ code-Chall/code_ChallApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_ChallApp.swift
+//  code-Chall
+//
+//  Created by Hamzah on 09.10.24.
+//
+
+import SwiftUI
+
+@main
+struct code_ChallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ code-ChallTests/code_ChallTests.swift
@@ -0,0 +1,17 @@
+//
+//  code_ChallTests.swift
+//  code-ChallTests
+//
+//  Created by Hamzah on 09.10.24.
+//
+
+import Testing
+@testable import code_Chall
+
+struct code_ChallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
